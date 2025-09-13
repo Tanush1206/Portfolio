6b6244a81a91aa6eedaf3a49e306ee1087d40104
tanush1206 <tanushthakran1206@gmail.com>
2025-09-14 02:11:03 +0530

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/PORTFOLIO/',   // must match your GitHub repo name
+  base: '/',   // must match your GitHub repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'], // optional, keep if you need it
